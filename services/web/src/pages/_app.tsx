0eb420b969da0900176011a7ef20c7b2ccf4e16d
kalekberov <kamil.alekberov@ps.kz>
2020-10-10 21:01:02 +0600

    feat: add antd and main layout

---
+++ services/web/src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MainLayout } from '../components/Layout';
+import '../styles/main.less';
+
+function app({ Component, pageProps }) {
+  return (
+  <MainLayout>
+      <Component {...pageProps} />;
+  </MainLayout>); 
+}
+
+export default app;
