1ba10dc850de795697cf2623b5c52e096fd21cd9
kalekberov <kamil.alekberov@ps.kz>
2020-10-25 19:09:21 +0600

    feat: add docker service

--- services/auth/src/helpers/checkEnv.ts
+++ services/auth/src/helpers/checkEnv.ts
@@ -1,36 +1,46 @@
-import {Application} from 'express';
+import { Application } from 'express';
 import dotenv from 'dotenv';
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-export function checkEnv (app: Application) {
-    dotenv.config({
-        path: require("path").resolve(process.cwd(), process.env.NODE_ENV !== 'prod' ? ".env.dev" : '.env'),
-    });
+export function checkEnv(app: Application) {
+  const envPath = require('path').resolve(
+    process.cwd(),
+    process.env.NODE_ENV !== 'prod' ? '.env.dev' : '.env'
+  );
 
-    if (process.env.NODE_ENV !== "prod") {
-        const morgan = require('morgan')
-        app.use(morgan("dev"));
-    }
-    
-    if (!process.env?.JWT_SECRET) {
-        throw new Error("JWT_SECRET env is not provided");
-    }
-    
-    if (!process.env.MONGO_URI) {
-        throw new Error("no mongo_uri env provided");
-    }
+  dotenv.config({
+    path: envPath,
+    debug: process.env.NODE_ENV !== 'prod',
+  });
 
-    (async () => {
-        try {
-          await mongoose.connect(process.env.MONGO_URI!, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-          });
-        } catch (error) {
-          console.log({ error });
-        }
-      })()
+  if (process.env.NODE_ENV !== 'prod') {
+    console.warn('\x1b[32m%s\x1b[0m', '[+] ENV file:', envPath);
+    const morgan = require('morgan');
+    app.use(morgan('dev'));
+  }
+
+  if (!process.env?.JWT_SECRET) {
+    throw new Error('JWT_SECRET env is not provided');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('no mongo_uri env provided');
+  }
+
+  mongoConnect();
 }
 
+async function mongoConnect() {
+  try {
+    const mongo = await mongoose.connect(process.env.MONGO_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+
+    console.warn('\x1b[32m%s\x1b[0m', '[+] Mongoose connected to DB:', mongo.connection.name);
+  } catch (error) {
+    console.error({ error });
+  }
+}
---
+++ services/docker/go.mod
@@ -0,0 +1,11 @@
+module kalekber/dream/services/docker
+
+go 1.15
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.8.0
+	go.mongodb.org/mongo-driver v1.4.2
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
