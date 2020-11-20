9e5351efdb245ed865de123d0e87c721a88719be
Kamil A <kamil.alekber@gmail.com>
2020-11-20 22:36:07 +0600

    fix: remove files

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,61 +1,41 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import { AppRoutes } from "./routes";
-import cors from "cors";
-import helmet from "helmet";
-import path from "path";
-import { checkEnv } from "./helpers/checkEnv";
-import favicon from "serve-favicon";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import { AppRoutes } from './routes';
+import cors from 'cors';
+import helmet from 'helmet';
+import path from 'path';
+import { checkEnv } from './helpers/checkEnv';
+import favicon from 'serve-favicon';
 const app = express();
 checkEnv(app);
 
 // app.set('trust proxy', 1);
-// Disable `powered-by` and add other protections
-// https://www.npmjs.com/package/helmet
-// app.use(favicon(path.join(__dirname, "public", "img", "favicon.ico")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
-app.set("views", path.resolve(__dirname, "public", "views"));
-app.set("view engine", "ejs");
 app.use(helmet());
 
 const whitelist =
-  process.env.NODE_ENV === "prod"
+  process.env.NODE_ENV === 'prod'
     ? []
-    : [
-        "http://localhost:3000",
-        "http://localhost:4000",
-        "http://localhost:5000",
-      ];
-      
+    : ['http://localhost:3000', 'http://localhost:4000', 'http://localhost:5000'];
+
 app.use(
   cors({
     credentials: true,
-    allowedHeaders: [
-      "Content-Type",
-      "Cookie",
-      "Origin",
-      "X-Requested-With",
-      "Accept",
-    ],
-    methods: ["GET", "HEAD", "POST", "OPTIONS"],
+    allowedHeaders: ['Content-Type', 'Cookie', 'Origin', 'X-Requested-With', 'Accept'],
+    methods: ['GET', 'HEAD', 'POST', 'OPTIONS'],
     origin: (origin: string | undefined, callback: any) => {
-      if (
-        whitelist.includes(`${origin}`) ||
-        typeof origin === "undefined" ||
-        origin === "null"
-      ) {
+      if (whitelist.includes(`${origin}`) || typeof origin === 'undefined' || origin === 'null') {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(new Error('Not allowed by CORS'));
       }
     },
   })
 );
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 app.use(AppRoutes);
 
@@ -70,9 +50,5 @@ app.use(AppRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.warn(
-    "\x1b[32m%s\x1b[0m",
-    "[+] 🚀 Sever is listening on:",
-    `http://localhost:${port}`
-  );
+  console.warn('\x1b[32m%s\x1b[0m', '[+] 🚀 Sever is listening on:', `http://localhost:${port}`);
 });
--- services/auth/src/routes/Main/mainRoutes.ts
+++ services/auth/src/routes/Main/mainRoutes.ts
@@ -1,7 +1,18 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
 const router = Router();
 
-router.route('/').get((req, res)=> res.status(200).render('pages/index', {title: 'Auth Service', serverUrl: process.env.serverUrl, workerUrl: process.env.workerUrl, webUrl: process.env.webUrl}))
+router
+  .route('/')
+  .get((req, res) =>
+    res
+      .status(200)
+      .render('pages/index', {
+        title: 'Auth Service',
+        serverUrl: process.env.serverUrl,
+        workerUrl: process.env.workerUrl,
+        webUrl: process.env.webUrl,
+      })
+  );
 
-export {router as MainRoutes};
+export { router as MainRoutes };
